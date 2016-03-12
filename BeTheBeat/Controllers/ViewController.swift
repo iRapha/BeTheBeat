be2f95e03ed0f15b4f65481dad3639304620e84b
Kevin Randrup <kevnrandrup@gmail.com>
2016-03-12 15:43:25 -0500

    ViewController picks a song to start out with

--- BeTheBeat/Controllers/ViewController.swift
+++ BeTheBeat/Controllers/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MediaPlayer
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var songAsset: AVURLAsset? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,32 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if songAsset == nil {
+            pickSong()
+        }
+    }
+    
+}
+
+extension ViewController : MPMediaPickerControllerDelegate {
+    private func pickSong() {
+        
+        let picker = MPMediaPickerController(mediaTypes: .Music)
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func mediaPicker(mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        if let song = mediaItemCollection.items.first,
+            let url = song.assetURL {
+                let asset = AVURLAsset(URL: url)
+                self.songAsset = asset
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
     }
 }
 
