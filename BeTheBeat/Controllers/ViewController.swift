8fcb1a6d6cbbcbc1498e86906dc5dc613ae042be
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-03-12 17:37:34 -0500

    add initial recorder logic

--- BeTheBeat/Controllers/ViewController.swift
+++ BeTheBeat/Controllers/ViewController.swift
@@ -24,11 +24,54 @@ class ViewController: UIViewController {
         }
     }
     
+    var tempRecordingURL: NSURL? = nil {
+        didSet {
+            print(tempRecordingURL)
+        }
+    }
+    var recorder: AVAudioRecorder? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setUpRecorder()
+    }
+    
+    func setUpRecorder() {
+        tempRecordingURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("tempSong")
+        let recordSettings: [String:AnyObject] = [AVEncoderAudioQualityKey: NSNumber(long: AVAudioQuality.Min.rawValue),
+            AVEncoderBitRateKey: NSNumber(long: 16),
+            AVNumberOfChannelsKey: NSNumber(long: 2),
+            AVSampleRateKey: NSNumber(floatLiteral: 44100.0)]
+        
+        do {
+            try recorder = AVAudioRecorder(URL: tempRecordingURL!, settings: recordSettings)
+            recorder?.prepareToRecord()
+            record10Sec()
+        } catch {
+            
+        }
+
     }
 
+    func record10Sec() {
+        recorder?.record()
+        var triggerTime = (Int64(NSEC_PER_SEC) * 10)
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, triggerTime), dispatch_get_main_queue(), { () -> Void in
+            stopRecording()
+        })
+    }
+    
+    func stopRecording() {
+        recorder?.stop()
+        getBPMForRecording()
+//uncomment when you feel confident it works        record10Sec()
+    }
+    
+    func getBPMForRecording() {
+        // TODO: Kevin :)
+    }
+    
     private func adjustBeat(originalBeat originalBeat: Float, newBeat: Float) {
         musicPlayer?.rate = (originalBeat / newBeat)
     }
